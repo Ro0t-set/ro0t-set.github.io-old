041625cbd1b6d54191834133122a95b0779aa69a
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-02-28 15:35:55 +0100

    feat(package.json): add vite-plugin-sitemap as a new dependency to generate sitemap for SEO purposes feat(vite.config.ts): configure vite-plugin-sitemap to generate sitemap with dynamic routes and external sitemaps for SEO optimization

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,25 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import Sitemap from 'vite-plugin-sitemap'
+
+const hostname = "https://tommasopatriti.me"
 
 export default defineConfig({
-  base:"./" ,
+  base: "./",
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
-
+    Sitemap({ 
+      hostname,
+      dynamicRoutes: [
+        "/multilanguage-portfolio-template"
+      ],
+      externalSitemaps: [
+        "https://www.tommasopatriti.me/PPS-24-ScafiWeb3/sitemap.xml"
+      ]
+    }),
     quasar({
       sassVariables: fileURLToPath(
         new URL('./src/quasar-variables.sass', import.meta.url)
