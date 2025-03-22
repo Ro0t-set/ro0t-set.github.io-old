4cf1bbc231f44c7abf96000a1b67ae19d638e37d
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-03-22 10:31:42 +0100

    feat(index.html): update language to Italian, add keywords and alternate locale for SEO optimization feat(index.html): enhance metadata and descriptions for SEO and social media sharing feat(index.html): add Google tag for analytics tracking feat(generalInfoTranslations.ts): update role to include location for better context

--- src/locale/translations/generalInfoTranslations.ts
+++ src/locale/translations/generalInfoTranslations.ts
@@ -3,13 +3,13 @@ import type { GeneralInfoTranslations } from "../locale";
 export const generalInfoEn: GeneralInfoTranslations = {
     name: "Tommaso",
     surname: "Patriti",
-    role: "Software Engineer",
+    role: "Freelancer Software Engineer | Bologna, Italy",
     description: "Passionate software engineer with experience in Kubernetes, distributed systems, and clean architecture. Interested in system scalability, cloud computing, and modern software development practices.",
 };
 
 export const generalInfoIt: GeneralInfoTranslations = {
     name: "Tommaso",
     surname: "Patriti",
-    role: "Ingegnere del Software",
+    role: "Freelancer Software Engineer | Bologna, Italy",
     description: "Ingegnere del software appassionato con esperienza in Kubernetes, sistemi distribuiti e clean architecture. Interessato alla scalabilità dei sistemi, al cloud computing e alle moderne pratiche di sviluppo software.",
 };
